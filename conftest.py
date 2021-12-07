72da3ac54c34b2bebcbc7001ad6fae7e68a05e62
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2021-12-07 13:49:39 +0000

    ui work

--- conftest.py
+++ conftest.py
@@ -12,4 +12,5 @@ def sents_chars_expected():
                            "",
                            "",
                            "",
-                        
+                           "",
+                           "",
