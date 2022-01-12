261d303ee2065c2e9246889d6d23880934466408
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-01-12 19:10:47 +0000

    Continuing

--- nlp_example.py
+++ nlp_example.py
@@ -7,4 +7,5 @@ from nlprov.preprocessing import preprocess_text
 from nlprov.vectorize import vectorize_text, vectorize_new_text
 from nlprov.similarity_calc import similarity_calculation
 
-text = pd.Series(da
+text = pd.Series(data=["  Combination  of   spaces.    ",
+            
