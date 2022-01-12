7fd63a4bf6385ca69475902fe932fb54afde3e00
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-01-12 05:51:44 +0000

    order

--- nlp_example.py
+++ nlp_example.py
@@ -5,4 +5,6 @@ Copyright © 2020 Johnson & Johnson
 import pandas as pd
 from nlprov.preprocessing import preprocess_text
 from nlprov.vectorize import vectorize_text, vectorize_new_text
-from nlprov.similarity_calc import simil
+from nlprov.similarity_calc import similarity_calculation
+
+text = pd.Series(da
