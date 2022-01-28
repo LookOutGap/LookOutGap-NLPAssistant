7a8430e1836f234183aa9d7bb1d96ae9d05dee5d
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-01-28 08:33:16 +0000

    9.0.1

--- nlprov/__init__.py
+++ nlprov/__init__.py
@@ -9,4 +9,4 @@ def get_spacy_nlp():
     try:
         spacy_nlp = en_core_web_sm.load(disable=['parser', 'ner', 'textcat'])
     except OSError:
-        # 
+        # We should tell the user explicitly what they nee
