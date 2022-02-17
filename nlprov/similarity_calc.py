f04d7423a96185c45e8e803e3b3f5931a3adfd7f
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-02-17 13:53:56 +0000

    Changed format

--- nlprov/similarity_calc.py
+++ nlprov/similarity_calc.py
@@ -5,4 +5,6 @@ Copyright © 2020 Johnson & Johnson
 import warnings
 from sklearn.metrics import pairwise_distances
 
-supported_metrics = ['cosine', 'jaccard', 'ma
+supported_metrics = ['cosine', 'jaccard', 'manhattan', 'dice', 'hamming']
+
+sparse_metrics = ['cityblo
