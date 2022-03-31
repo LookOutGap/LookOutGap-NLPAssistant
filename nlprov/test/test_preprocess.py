f710e144af39395945c31147fa3a65dd17a115d4
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-03-31 11:58:39 +0000

    intelli_plug: older Qualcomm kernel compatibility fixup

--- nlprov/test/test_preprocess.py
+++ nlprov/test/test_preprocess.py
@@ -5,4 +5,5 @@ Copyright © 2020 Johnson & Johnson
 import pytest
 import pandas as pd
 import numpy as np
-from nlprov.preprocessing import prep
+from nlprov.preprocessing import preprocess_text
+from conftest import sents_chars_expected, se
