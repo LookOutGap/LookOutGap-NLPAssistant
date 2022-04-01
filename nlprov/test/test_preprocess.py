2cb459a692a42a8e69b7558a13405c6e9866374f
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-04-01 07:20:17 +0000

    2.0.20

--- nlprov/test/test_preprocess.py
+++ nlprov/test/test_preprocess.py
@@ -6,4 +6,8 @@ import pytest
 import pandas as pd
 import numpy as np
 from nlprov.preprocessing import preprocess_text
-from conftest import sents_chars_expected, se
+from conftest import sents_chars_expected, sents_nums_expected, \
+    sents_all_expected
+
+
+# Creating data f
