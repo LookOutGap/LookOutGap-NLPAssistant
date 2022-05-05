fa4975960c27c95ee5fec50b91a6c5c27a8bc488
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-05-05 17:21:43 +0000

    update patch version

--- nlprov/test/test_preprocess.py
+++ nlprov/test/test_preprocess.py
@@ -61,4 +61,5 @@ def lowercase_expected():
 # Testing lowercase
 def test_lowercase(lowercase_actual, lowercase_expected):
     preprocessed = preprocess_text(lowercase_actual,
-      
+                                   regex='(?!).*',
+                  
