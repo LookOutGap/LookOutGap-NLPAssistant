3c7abdf37245bd712e03f18fb4f8bff8aab556e7
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-06-09 16:33:26 +0000

    2.12.2

--- nlprov/test/test_preprocess.py
+++ nlprov/test/test_preprocess.py
@@ -116,4 +116,5 @@ def sents_default_expected():
 
 # Testing the default regex preprocess
 def test_regex_default(sents_regex, sents_default_expected):
-    sen
+    sents_preprocessed = preprocess_text(sents_regex,
+ 
