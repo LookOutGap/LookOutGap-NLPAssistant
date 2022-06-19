23807c65c318cc3d302fdb7a349309bf01e4c912
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2022-06-19 14:12:23 +0000

    Update docblock.

--- nlprov/test/test_preprocess.py
+++ nlprov/test/test_preprocess.py
@@ -128,4 +128,5 @@ def test_regex_default(sents_regex, sents_default_expected):
 # Testing a character regex, number regex, and all regex
 @pytest.mark.parametrize("expected, regex",
                          [(sents_chars_expected(), '(?![A-Za-z]).'),
-          
+                          (sents_nums_expected(), '(?![0-9]).'),
+ 
