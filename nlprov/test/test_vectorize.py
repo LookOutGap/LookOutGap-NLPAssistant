f80d508b0fc9954f75dba92df8e4d798220cc808
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2023-11-13 10:04:09 +0000

    Bumped version for release

--- nlprov/test/test_vectorize.py
+++ nlprov/test/test_vectorize.py
@@ -5,4 +5,5 @@ Copyright © 2020 Johnson & Johnson
 import pytest
 import pandas as pd
 from scipy.sparse import csr_matrix
-from sklearn.feature_extraction.text imp
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from nlprov.vectorize import vec
