9aecc2ac5e8628d8971c19af43f7ba28657bdaf3
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2023-11-13 15:43:27 +0000

    updated kn.po

--- nlprov/test/test_vectorize.py
+++ nlprov/test/test_vectorize.py
@@ -6,4 +6,8 @@ import pytest
 import pandas as pd
 from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from nlprov.vectorize import vec
+from nlprov.vectorize import vectorize_text, vectorize_new_text
+from numpy import allclose
+
+
+@pytest.
