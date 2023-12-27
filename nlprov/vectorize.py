6a7bf2c1f36a3bdd71a54c5f80ba0770865505cd
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2023-12-27 13:32:14 +0000

    service worker

--- nlprov/vectorize.py
+++ nlprov/vectorize.py
@@ -3,4 +3,7 @@ Copyright © 2020 Johnson & Johnson
 """
 
 import pandas as pd
-from sklearn.feature_extraction.text i
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
+def vectorize
