53785d26a858ed088f2ff4ece209379de4da3c90
LookOutGap <131667368+LookOutGap@users.noreply.github.com>
2023-12-29 14:05:52 +0000

    yum

--- nlprov/vectorize.py
+++ nlprov/vectorize.py
@@ -7,4 +7,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 def vectorize_text(text_col: pd.Series,
-                   vec_typ
+                   vec_type: str = 'count',
+                   **kwargs):
+    """
+    Vectoriz
